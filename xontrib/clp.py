8176cecff553df1097000e1bc2cf3d4dc2d9bdfc
Andy Kipp <anki-code@users.noreply.github.com>
2023-02-01 11:25:33 +0600

    Update and rename jump_to_dir.py to clp.py

---
+++ xontrib/clp.py
@@ -0,0 +1,19 @@
+import shutil as _shutil
+
+if _shutil.which('pbcopy'):  # DARWIN
+    aliases['clp'] = 'pbcopy'
+elif _shutil.which('xclip'):  # LINUX
+    aliases['clp'] = 'xclip -sel clip'
+elif _shutil.which('clip.exe'):  # WINDOWS
+    aliases['clp'] = 'clip.exe'
+elif _shutil.which('CLIP.EXE'):  # WINDOWS
+    aliases['clp'] = 'CLIP.EXE'
+else:
+    from xonsh.platform import ON_LINUX, ON_DARWIN #, ON_WINDOWS, ON_WSL, ON_CYGWIN, ON_MSYS, ON_POSIX, ON_FREEBSD, ON_DRAGONFLY, ON_NETBSD, ON_OPENBSD
+    if ON_LINUX:
+        print('xontrib-clp: install xclip')
+    elif ON_DARWIN:
+        print('xontrib-clp: install pbcopy')
+    elif ON_WINDOWS:
+        print('xontrib-clp: clip.exe not found')
+        
